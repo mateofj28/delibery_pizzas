efd5976809b64b6f086218e3d03e394b43401623
mateo florez jaramillo <mateo.florez.9594@eam.edu.co>
2024-06-04 15:05:54 -0500

    add change

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:delibery_pizzas/providers/pizzas_providers.dart';
 import 'package:delibery_pizzas/screens/create_product.dart';
 import 'package:delibery_pizzas/screens/create_repartidor.dart';
 import 'package:delibery_pizzas/screens/home.dart';
@@ -9,9 +10,15 @@ import 'package:delibery_pizzas/screens/my_profile.dart';
 import 'package:delibery_pizzas/screens/signUp.dart';
 import 'package:delibery_pizzas/screens/welcome.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(
+    const MyApp()
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -20,25 +27,30 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => PizzasProviders())
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        initialRoute: '/',
+        routes: {
+          '/': (context) => WelcomeScreen(),
+          '/login': (context) => LoginScreen(),
+          '/signUp': (context) => SignUpScreen(),
+          '/home': (context) => HomeScreen(),
+          '/menu': (context) => MenuScreen(),
+          '/myProfile': (context) => MyProfileScreen(),
+          '/homeAdmin': (context) => HomeAdminScreen(),
+          '/menuAdmin': (context) => MenuAdminScreen(),
+          '/createProduct': (context) => CreateProductScreen(),
+          '/createRepartidor': (context) => CreateRepartidorScreen(),
+        },
       ),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => WelcomeScreen(),
-        '/login': (context) => LoginScreen(),
-        '/signUp': (context) => SignUpScreen(),
-        '/home': (context) => HomeScreen(),
-        '/menu': (context) => MenuScreen(),
-        '/myProfile': (context) => MyProfileScreen(),
-        '/homeAdmin': (context) => HomeAdminScreen(),
-        '/menuAdmin': (context) => MenuAdminScreen(),
-        '/createProduct': (context) => CreateProductScreen(),
-        '/createRepartidor': (context) => CreateRepartidorScreen(),
-      },
     );
   }
 }
